8b42926caf582822c16fffff69541a4fdd5fd673
MarinaTod <todorishina_marina@mail.ru>
2017-04-28 01:27:37 +0300

    Fixed db connection and changed form

--- database.php
+++ database.php
@@ -14,5 +14,5 @@ define('DB_CHARSET', 'utf8');
 
 
 
-$link = mysqli_connect('DB_HOST', 'DB_USER', 'DB_PASSWORD', 'DB_NAME');
+$link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 mysqli_set_charset($link, DB_CHARSET);
--- form.php
+++ form.php
@@ -4,11 +4,17 @@
 
 <h1>Sum form</h1>
 <form method="post" name="sum" action="<?php echo $_SERVER['PHP_SELF'];?>">
-    a <input type="text" name="a">
-    +
-    b <input type="text" name="b">
+    <label for="login">Login
+    <input type="text" name="login">
+    </label>
+    <label for="mail">E-mail
+    <input type="text" name="mail">
+    </label>
+    <label for="passw">Password
+    <input type="text" name="passw">
+    </label>
     <button type="submit" >
-    =
+    Submit
     </button>
 </form>
 
