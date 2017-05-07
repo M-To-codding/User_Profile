7cbc254e21a77524bf8bf20862d8a074631a76cb
Todoryshyna-Maryna <todorishina.marina@mail.ru>
2017-05-07 22:05:51 +0300

    Changed BlogController content

--- controllers/BlogController.php
+++ controllers/BlogController.php
@@ -11,7 +11,32 @@ class BlogController
 
     public function actionIndex() {
 
-        echo 'BlogController  actionIndex';
+        echo '<h1>List of blog</h1><br/>
+           <h3>Heading1</h3>
+           <p>Lorem ipsum dolor sit amet, 
+           consectetur adipisicing elit.<br/> 
+           Adipisci asperiores autem beatae 
+           blanditiis culpa, debitis error <br/>
+           inventore ipsum itaque iure, magnam</p>
+           <a href="#">Read more ></a>
+           <h3>Heading2</h3>
+           <p>Lorem ipsum dolor sit amet, 
+           consectetur adipisicing elit. <br/>
+           Adipisci asperiores autem beatae 
+           blanditiis culpa, debitis error <br/>
+           inventore ipsum itaque iure, magnam</p>
+           <a href="#">Read more ></a>';
+        return true;
+    }
+
+    public function actionView(){
+        echo '<h3>Heading</h3>
+           <p>Lorem ipsum dolor sit amet, 
+           consectetur adipisicing elit.<br/> 
+           Adipisci asperiores autem beatae 
+           blanditiis culpa, debitis error <br/>
+           inventore ipsum itaque iure, magnam</p>
+           <a href="#">< Back</a>';
         return true;
     }
 }
--- index.php
+++ index.php
@@ -17,14 +17,5 @@ $router->run();
 
 //require_once('db_connect/database.php');
 
-//class Main
-//{
-//    static public function callForm()
-//    {
-//        return include('templates/form.php');
-//    }
-//}
-//
-//echo Main::callForm();
 
 ?>
