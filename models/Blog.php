b2f091ad8ccd03f4caef0f8f9258a7cee35ec3ab
Todoryshyna-Maryna <todorishina.marina@mail.ru>
2017-05-09 06:00:51 +0300

    Added models for records in blog

---
+++ models/Blog.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Я
+ * Date: 09.05.2017
+ * Time: 5:52
+ */
+
+class Blog
+{
+
+
+    public static function getRecordsItemById($id){
+
+    }
+
+    public static function getRecordsList() {
+
+    }
+
+}
