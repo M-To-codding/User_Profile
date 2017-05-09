acfa58899cc5d48424aeec1b8e9d1d173a24365a
Todoryshyna-Maryna <todorishina.marina@mail.ru>
2017-05-09 06:06:01 +0300

    Included model Blog into controller and used its methods

--- controllers/BlogController.php
+++ controllers/BlogController.php
@@ -6,39 +6,64 @@
  * Date: 07.05.2017
  * Time: 1:55
  */
+
+include_once ROOT. '/models/Blog.php';
+
 class BlogController
 {
 
     public function actionIndex() {
 
-        echo '<h1>List of blog</h1><br/>
-           <h3>Heading1</h3>
-           <p>Lorem ipsum dolor sit amet, 
-           consectetur adipisicing elit.<br/> 
-           Adipisci asperiores autem beatae 
-           blanditiis culpa, debitis error <br/>
-           inventore ipsum itaque iure, magnam</p>
-           <a href="#">Read more ></a>
-           <h3>Heading2</h3>
-           <p>Lorem ipsum dolor sit amet, 
-           consectetur adipisicing elit. <br/>
-           Adipisci asperiores autem beatae 
-           blanditiis culpa, debitis error <br/>
-           inventore ipsum itaque iure, magnam</p>
-           <a href="#">Read more ></a>';
+
+        $recordsList = array();
+        $recordsList = Blog::getRecordsList();
+
+
+        echo '<pre>';
+        print_r($recordsList);
+        echo '</pre>';
+
+
+//        echo '<h1>List of blog</h1><br/>
+//           <h3>Heading1</h3>
+//           <p>Lorem ipsum dolor sit amet,
+//           consectetur adipisicing elit.<br/>
+//           Adipisci asperiores autem beatae
+//           blanditiis culpa, debitis error <br/>
+//           inventore ipsum itaque iure, magnam</p>
+//           <a href="#">Read more ></a>
+//           <h3>Heading2</h3>
+//           <p>Lorem ipsum dolor sit amet,
+//           consectetur adipisicing elit. <br/>
+//           Adipisci asperiores autem beatae
+//           blanditiis culpa, debitis error <br/>
+//           inventore ipsum itaque iure, magnam</p>
+//           <a href="#">Read more ></a>';
         return true;
     }
 
     public function actionView($category, $id){
-        echo '<h3>Heading</h3>
-           <p>Lorem ipsum dolor sit amet, 
-           consectetur adipisicing elit.<br/> 
-           Adipisci asperiores autem beatae 
-           blanditiis culpa, debitis error <br/>
-           inventore ipsum itaque iure, magnam</p>
-           <a href="#">< Back</a>';
-        echo '<br>'.$category.
-            '<br>'.$id;
+
+        if($id) {
+            $recordsItem = Blog::getRecordsItemById($id);
+
+            echo '<pre>';
+            print_r($recordsItem);
+            echo '</pre>';
+
+            echo 'actionView';
+
+        }
+
+//        echo '<h3>Heading</h3>
+//           <p>Lorem ipsum dolor sit amet,
+//           consectetur adipisicing elit.<br/>
+//           Adipisci asperiores autem beatae
+//           blanditiis culpa, debitis error <br/>
+//           inventore ipsum itaque iure, magnam</p>
+//           <a href="#">< Back</a>';
+//        echo '<br>'.$category.
+//            '<br>'.$id;
 
         return true;
     }
