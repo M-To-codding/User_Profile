e3e56f705519fe1d12c635a4cf4ef492788069cd
Todoryshyna-Maryna <todorishina.marina@mail.ru>
2017-06-07 11:50:48 +0300

    Added form for creating new records

--- views/blog/addingRecordsPage.php
+++ views/blog/addingRecordsPage.php
@@ -13,8 +13,17 @@ require_once(ROOT . '/views/including/_include_header.php');
             </div>
 
             <form action="" method="post"  class="blog-block">
-                <label for="title">Title</label>
-                <input type="text" id="title">
+                <div>
+                    <label for="title">Add title</label>
+                    <input type="text" id="title"/>
+                </div>
+                <div>
+                    <label for="content">Add content</label>
+                    <textarea id="content" name="content"></textarea>
+                </div>
+                <button type="submit">
+                    Save
+                </button>
             </form>
         </section>
     </main>
